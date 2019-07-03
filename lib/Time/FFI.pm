f1109b6113db02e8f2f79dbe51bd62f5de615645
Dan Book <grinnz@grinnz.com>
2019-07-03 17:22:54 -0400

    initial implementation

--- lib/Time/FFI.pm
+++ lib/Time/FFI.pm
@@ -2,18 +2,224 @@ package Time::FFI;
 
 use strict;
 use warnings;
+use Carp 'croak';
+use Exporter 'import';
+use FFI::Platypus;
+use FFI::Platypus::Buffer;
+use FFI::Platypus::Memory;
+use Time::FFI::tm;
 
 our $VERSION = '0.001';
+
+our @EXPORT_OK = qw(asctime ctime gmtime localtime mktime strftime strptime);
+our %EXPORT_TAGS = (all => \@EXPORT_OK);
+
+my $ffi = FFI::Platypus->new(lib => [undef], ignore_not_found => 1);
+$ffi->type('record(Time::FFI::tm)' => 'tm');
+my $char_size = $ffi->sizeof('char');
+
+if (defined $ffi->find_symbol('asctime_r')) {
+  $ffi->attach([asctime_r => 'asctime'] => ['tm', 'opaque'] => 'string' => sub {
+    my ($xsub, $tm) = @_;
+    my $rc = $xsub->($tm, my $buf = calloc(26, $char_size));
+    free $buf;
+    croak "$!" unless defined $rc;
+    return $rc;
+  });
+} else {
+  $ffi->attach(asctime => ['tm'] => 'string' => sub {
+    my ($xsub, $tm) = @_;
+    my $rc = $xsub->($tm);
+    croak "$!" unless defined $rc;
+    return $rc;
+  });
+}
+
+if (defined $ffi->find_symbol('ctime_r')) {
+  $ffi->attach([ctime_r => 'ctime'] => ['time_t*', 'opaque'] => 'string' => sub {
+    my ($xsub, $time) = @_;
+    $time = time unless defined $time;
+    my $rc = $xsub->(\$time, my $buf = calloc(26, $char_size));
+    free $buf;
+    croak "$!" unless defined $rc;
+    return $rc;
+  });
+} else {
+  $ffi->attach(ctime => ['time_t*'] => 'string' => sub {
+    my ($xsub, $time) = @_;
+    $time = time unless defined $time;
+    my $rc = $xsub->(\$time);
+    croak "$!" unless defined $rc;
+    return $rc;
+  });
+}
+
+if (defined $ffi->find_symbol('gmtime_r')) {
+  $ffi->attach([gmtime_r => 'gmtime'] => ['time_t*', 'tm'] => 'opaque' => sub {
+    my ($xsub, $time) = @_;
+    $time = time unless defined $time;
+    my $rc = $xsub->(\$time, my $tm = Time::FFI::tm->new);
+    croak "$!" unless defined $rc;
+    return $tm;
+  });
+} else {
+  $ffi->attach(gmtime => ['time_t*'] => 'tm' => sub {
+    my ($xsub, $time) = @_;
+    $time = time unless defined $time;
+    my $rc = $xsub->(\$time);
+    croak "$!" unless defined $rc;
+    return $rc;
+  });
+}
+
+if (defined $ffi->find_symbol('localtime_r')) {
+  $ffi->attach([localtime_r => 'localtime'] => ['time_t*', 'tm'] => 'opaque' => sub {
+    my ($xsub, $time) = @_;
+    $time = time unless defined $time;
+    my $rc = $xsub->(\$time, my $tm = Time::FFI::tm->new);
+    croak "$!" unless defined $rc;
+    return $tm;
+  });
+} else {
+  $ffi->attach(localtime => ['time_t*'] => 'tm' => sub {
+    my ($xsub, $time) = @_;
+    $time = time unless defined $time;
+    my $rc = $xsub->(\$time);
+    croak "$!" unless defined $rc;
+    return $rc;
+  });
+}
+
+$ffi->attach(mktime => ['tm'] => 'time_t' => sub {
+  my ($xsub, $tm) = @_;
+  my $rc = $xsub->($tm);
+  croak "$!" if $rc == -1;
+  return $rc;
+});
+
+$ffi->attach(strftime => ['opaque', 'size_t', 'string', 'tm'] => 'size_t' => sub {
+  my ($xsub, $format, $tm) = @_;
+  my $max_size = length($format) * 20;
+  my $buf_size = 200;
+  my $rc = 0;
+  my $buf;
+  until ($rc != 0) {
+    free $buf if defined $buf;
+    $rc = $xsub->($buf = calloc($buf_size, $char_size), $buf_size, $format, $tm);
+    last if $buf_size > $max_size;
+  } continue {
+    $buf_size *= 2;
+  }
+  my $str = buffer_to_scalar $buf, $rc * $char_size;
+  free $buf;
+  return $str;
+});
+
+$ffi->attach(strptime => ['string', 'string', 'tm'] => 'string' => sub {
+  my ($xsub, $str, $format, $tm, $remaining) = @_;
+  $tm = Time::FFI::tm->new unless defined $tm;
+  my $rc = $xsub->($str, $format, $tm);
+  croak "$!" unless defined $rc;
+  $$remaining = $rc if defined $remaining;
+  return $tm;
+});
 
 1;
 
 =head1 NAME
 
-Time::FFI - Module abstract
+Time::FFI - Date and time transformation functions via libffi
 
 =head1 SYNOPSIS
 
+  use Time::FFI qw(localtime mktime strptime strftime);
+  use Time::FFI::tm;
+
+  my $tm = localtime time;
+  my $epoch = mktime $tm;
+  my $tm = strptime '1995-01-02 13:15:39', '%Y-%m-%d %H:%M:%S';
+  print strftime '%I:%M:%S %p on %B %e, %Y', $tm;
+
 =head1 DESCRIPTION
+
+B<Time::FFI> provides a L<libffi|FFI::Platypus> interface to POSIX date and
+time functions found in F<time.h>.
+
+The L</gmtime> and L</localtime> functions behave very differently from the
+core functions of the same name, as well as those exported by L<Time::Piece>,
+so you may wish to call them as e.g. C<Time::FFI::gmtime> rather than importing
+them.
+
+All functions will throw an exception containing the syscall error message in
+the event of an error. L<perlvar/$!> will also have been set by the syscall, so
+you could check it after trapping the exception for finer exception handling.
+
+=head1 FUNCTIONS
+
+All functions are exported on demand, or with the C<:all> export tag.
+
+=head1 asctime
+
+  my $str = asctime $tm;
+
+Returns a string in the format C<Wed Jun 30 21:49:08 1993\n> representing the
+passed L<Time::FFI::tm> record.
+
+=head1 ctime
+
+  my $str = ctime $epoch;
+  my $str = ctime;
+
+Returns a string in the format C<Wed Jun 30 21:49:08 1993\n> representing the
+passed epoch timestamp (defaulting to the current time) in the local time zone.
+
+=head1 gmtime
+
+  my $tm = gmtime $epoch;
+  my $tm = gmtime;
+
+Returns a L<Time::FFI::tm> record representing the passed epoch timestamp
+(defaulting to the current time) in UTC.
+
+=head1 localtime
+
+  my $tm = localtime $epoch;
+  my $tm = localtime;
+
+Returns a L<Time::FFI::tm> record representing the passed epoch timestamp
+(defaulting to the current time) in the local time zone.
+
+=head1 mktime
+
+  my $epoch = mktime $tm;
+
+Returns the epoch timestamp representing the passed L<Time::FFI::tm> record
+interpreted in the local time zone.
+
+=head1 strftime
+
+  my $str = strftime $format, $tm;
+
+Returns a string formatted according to the passed format string, representing
+the passed L<Time::FFI::tm> record. Consult your system's L<strftime(3)> manual
+for available format descriptors.
+
+=head1 strptime
+
+  my $tm = strptime $str, $format;
+     $tm = strptime $str, $format, $tm;
+  my $tm = strptime $str, $format, undef, \my $remaining;
+     $tm = strptime $str, $format, $tm, \my $remaining;
+
+Returns a L<Time::FFI::tm> record representing the passed string, parsed
+according to the passed format. Consult your system's L<strptime(3)> manual for
+available format descriptors.
+
+A L<Time::FFI::tm> record may be passed as the third argument, in which case it
+will be modified in place to (on most systems) update only the date/time
+elements which were parsed from the string. Additionally, an optional scalar
+reference may be passed as the fourth argument, in which case it will be set to
+the remaining unprocessed characters of the input string if any.
 
 =head1 BUGS
 
@@ -33,3 +239,4 @@ This is free software, licensed under:
 
 =head1 SEE ALSO
 
+L<Time::Piece>, L<Time::Moment>, L<DateTime>, L<POSIX>, L<POSIX::strptime>
---
+++ lib/Time/FFI/tm.pm
@@ -0,0 +1,117 @@
+package Time::FFI::tm;
+
+use strict;
+use warnings;
+use FFI::Platypus::Record;
+
+our $VERSION = '0.001';
+
+my @tm_members = qw(tm_sec tm_min tm_hour tm_mday tm_mon tm_year tm_wday tm_yday tm_isdst);
+
+record_layout(map { (int => $_) } @tm_members);
+
+sub from_list {
+  my ($class, @args) = @_;
+  my %attr;
+  $attr{$tm_members[$_]} = $args[$_] for 0..$#tm_members;
+  return $class->new(\%attr);
+}
+
+sub to_list {
+  my ($self) = @_;
+  return map { $self->$_ } @tm_members;
+}
+
+1;
+
+=head1 NAME
+
+Time::FFI::tm - POSIX tm structure
+
+=head1 SYNOPSIS
+
+  use Time::FFI::tm;
+
+  my $tm = Time::FFI::tm->new(
+    tm_year => 95, # years since 1900
+    tm_mon  => 0,  # 0 == January
+    tm_mday => 1,
+    tm_hour => 13,
+    tm_min  => 25,
+    tm_sec  => 59,
+  );
+
+  my $tm = Time::FFI::tm->from_list(localtime(time));
+
+  my $epoch = POSIX::mktime($tm->to_list);
+
+=head1 DESCRIPTION
+
+This L<FFI::Platypus::Record> class represents a time structure as used by
+functions such as L<mktime(3)> and L<strptime(3)>.
+
+=head1 ATTRIBUTES
+
+=head2 tm_sec
+
+=head2 tm_min
+
+=head2 tm_hour
+
+=head2 tm_mday
+
+=head2 tm_mon
+
+=head2 tm_year
+
+=head2 tm_wday
+
+=head2 tm_yday
+
+=head2 tm_isdst
+
+The integer components of the C<tm> struct are stored as settable attributes.
+
+=head1 METHODS
+
+=head2 new
+
+  my $tm = Time::FFI::tm->new;
+  my $tm = Time::FFI::tm->new(tm_year => $year, ...);
+  my $tm = Time::FFI::tm->new({tm_year => $year, ...});
+
+Construct a new B<Time::FFI::tm> object representing a C<tm> struct.
+
+=head2 from_list
+
+  my $tm = Time::FFI::tm->from_list($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst);
+
+Construct a new B<Time::FFI::tm> object from the passed list of values, in the
+same order returned by L<perlfunc/localtime>.
+
+=head2 to_list
+
+  my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = $tm->to_list;
+
+Return the list of values in the structure, in the same order returned by
+L<perlfunc/localtime>.
+
+=head1 BUGS
+
+Report any issues on the public bugtracker.
+
+=head1 AUTHOR
+
+Dan Book <dbook@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2019 by Dan Book.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+=head1 SEE ALSO
+
+L<Time::FFI>
